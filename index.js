7ecfbbf23ed972c46f8e17e5835f16caaf3c7760
rugvedp <74641670+rugvedp@users.noreply.github.com>
2021-06-26 17:43:50 +0530

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    unixCode: async (UNIX_TIMESTAMP) => {
+        var a = new Date(UNIX_TIMESTAMP * 1000);
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var time = date + ' ' + month + ' ' + year;
+        return time;
+    }
+}
